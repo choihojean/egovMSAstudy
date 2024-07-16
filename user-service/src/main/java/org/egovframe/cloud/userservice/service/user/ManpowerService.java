b9531a07226f78c75d99309a29b28373ec8432ef
choihojean <chlghwls1231@naver.com>
2024-07-16 17:39:48 +0900

    refactor : 권한수정진행중

--- user-service/src/main/java/org/egovframe/cloud/userservice/service/user/ManpowerService.java
+++ user-service/src/main/java/org/egovframe/cloud/userservice/service/user/ManpowerService.java
@@ -1,11 +1,12 @@
 package org.egovframe.cloud.userservice.service.user;
 
 import lombok.RequiredArgsConstructor;
-import org.egovframe.cloud.userservice.domain.user.Manpower;
-import org.egovframe.cloud.userservice.domain.user.ManpowerRepository;
 import org.egovframe.cloud.userservice.api.user.dto.ManpowerSaveRequestDto;
 import org.egovframe.cloud.userservice.api.user.dto.ManpowerResponseDto;
+import org.egovframe.cloud.userservice.domain.user.Manpower;
+import org.egovframe.cloud.userservice.domain.user.ManpowerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +14,7 @@ public class ManpowerService {
 
     private final ManpowerRepository manpowerRepository;
 
+    @Transactional
     public Long save(ManpowerSaveRequestDto requestDto) {
         Manpower manpower = requestDto.toEntity();
         manpowerRepository.save(manpower);
@@ -20,10 +22,8 @@ public class ManpowerService {
     }
 
     public ManpowerResponseDto findById(Long id) {
-        Manpower manpower = manpowerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("No manpower found with id " + id));
-        return new ManpowerResponseDto(manpower);
+        Manpower entity = manpowerRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 인재가 없습니다. id=" + id));
+        return new ManpowerResponseDto(entity);
     }
-
-    // Update, delete methods can be added similarly
 }
